9f43c1f8e36a8c418a85cb44a6e468153348c9ec
sokolprame <kstrin14t27@mail.ru>
2025-06-10 19:50:52 +0300

    23212

--- grpz_inventory/audit/admin.py
+++ grpz_inventory/audit/admin.py
@@ -1,5 +1,14 @@
-# audit/admin.py
+﻿# audit/admin.py
 from django.contrib import admin
 from .models import AuditLog
 
-admin.site.register(AuditLog)
+@admin.register(AuditLog)
+class AuditLogAdmin(admin.ModelAdmin):
+    list_display = ('user', 'action', 'timestamp', 'short_details')
+    list_filter = ('user', 'timestamp')
+    search_fields = ('user__username', 'action', 'details', 'old_value', 'new_value')
+    readonly_fields = ('user', 'action', 'timestamp', 'details', 'old_value', 'new_value')
+
+    def short_details(self, obj):
+        return (obj.details[:50] + '...') if obj.details and len(obj.details) > 50 else obj.details
+    short_details.short_description = 'Подробности'
